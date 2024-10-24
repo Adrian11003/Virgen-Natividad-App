285a955bc1f23754dfa93557ca74cb0cd0c22395
yKoult <145145645+yKoutl@users.noreply.github.com>
2024-10-24 19:54:40 +0000

    login theme

--- src/auth/index.jsx
+++ src/auth/index.jsx
@@ -1,64 +1,174 @@
-import { SafeAreaView, TextInput, View, Image, Text, Pressable, ActivityIndicator } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { SafeAreaView, TextInput, View, Image, Text, Pressable, ActivityIndicator, useWindowDimensions, ImageBackground } from 'react-native';
 import { AuthContext } from '../core/context/authContext';
 import { useNavigation } from '@react-navigation/native';
-import { useState, useContext } from 'react';
+import { useTheme } from '../core/context/themeContext';  // Ajusta la ruta del ThemeContext
 
 export const LoginScreen = () => {
   const [identificador, setIdentificador] = useState('');
   const [contrasena, setContrasena] = useState('');
   const { handleLogin, loading } = useContext(AuthContext);
   const navigation = useNavigation();
+  const { width } = useWindowDimensions();
+
+  const { theme, toogleThemeType, isDarkTheme } = useTheme();  // Extraemos el tema y el toggle
+
+  // Definir punto de quiebre para pantallas medianas (tabletas)
+  const isMediumScreen = width >= 768;
 
   if (loading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#000" />
+        <ActivityIndicator size="large" color={theme.colors.primary} />
       </View>
     );
   }
 
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', backgroundColor: '#f3f4f6' }}>
-      <View style={{ alignItems: 'center', padding: 24 }}>
-        <Text style={{ marginBottom: 16, fontSize: 36, textAlign: 'center', color: '#0f172a', fontWeight: 'bold' }} >
-          ¡Bienvenido!
-        </Text>
-        <Image
-          source={require("../assets/images/logo.png")}
-          style={{ width: 96, height: 96, marginBottom: 20 }}
-          resizeMode="contain"
-        />
-        <View style={{ width: '100%', marginBottom: 24 }}>
-          <TextInput
-            value={identificador}
-            onChangeText={setIdentificador}
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <View style={{
+        flexDirection: isMediumScreen ? 'row' : 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        backgroundColor: theme.colors.background, // Usar color del tema
+      }}>
+        {/* Sección del logo */}
+        <View style={{
+          flex: isMediumScreen ? 1 : 0,
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: isMediumScreen ? '40%' : '100%',
+          height: isMediumScreen ? '100%' : 'auto',
+          backgroundColor: isMediumScreen ? 'transparent' : theme.colors.surface
+        }}>
+          {isMediumScreen ? (
+            <ImageBackground
+              source={require("../assets/images/fondaco.jpg")}
+              style={{
+                flex: 5,
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '220%',
+                height: '100%',
+                padding: 80
+              }}
+              resizeMode="cover"
+            >
+              <Image
+                source={require("../assets/images/logo.png")}
+                style={{ width: 100, height: 100, marginBottom: 20 }}
+                resizeMode="contain"
+              />
+              <Text style={{ color: 'white', fontSize: 36, fontWeight: 'bold', textAlign: 'center', marginLeft: '20%' }}>
+                Virgen Natividad
+              </Text>
+            </ImageBackground>
+          ) : (
+            <Image
+              source={require("../assets/images/logo.png")}
+              style={{ width: 100, height: 100, marginBottom: 20 }}
+              resizeMode="contain"
+            />
+          )}
+        </View>
+
+        <View style={{
+          flex: isMediumScreen ? 2 : 1,
+          alignItems: 'center',
+          width: isMediumScreen ? '60%' : '100%',
+          padding: 24,
+          backgroundColor: theme.colors.surface,  // Usar color del tema
+          borderRadius: isMediumScreen ? 16 : 0,
+          justifyContent: 'center',
+          borderWidth: isMediumScreen ? 4 : 0,
+          borderColor: isMediumScreen ? theme.colors.border : 'transparent',
+          marginLeft: isMediumScreen ? 100 : 0,
+          marginRight: isMediumScreen ? 100 : 0,
+        }}>
+          <Text style={{
+            marginBottom: 16,
+            fontSize: isMediumScreen ? 40 : 36,
+            textAlign: 'center',
+            color: isDarkTheme ? theme.colors.text : theme.colors.primary,  // Usar color del tema
+            fontWeight: 'bold'
+          }}
+          onPress={toogleThemeType}
+          >
+            ¡Bienvenido de nuevo!
+          </Text>
+
+          <View style={{ width: '100%', marginBottom: 24 }}>
+            <TextInput
+              value={identificador}
+              onChangeText={setIdentificador}
+              style={{
+                width: '100%',
+                marginBottom: 12,
+                backgroundColor: theme.colors.surface,
+                borderWidth: 1,
+                borderColor: theme.colors.border,
+                padding: 12,
+                borderRadius: 16,
+                color: isDarkTheme ? theme.colors.text : theme.colors.primary  // Usar color del tema
+              }}
+              onPress={toogleThemeType}
+              placeholder="Nro. Documento"
+              placeholderTextColor={ theme.colors.placeholder}  // Placeholder acorde al tema
+            />
+            <TextInput
+              value={contrasena}
+              onChangeText={setContrasena}
+              style={{
+                width: '100%',
+                marginBottom: 12,
+                backgroundColor: theme.colors.surface,
+                borderWidth: 1,
+                borderColor: theme.colors.border,
+                padding: 12,
+                borderRadius: 16,
+                color:  isDarkTheme ? theme.colors.text : theme.colors.primary
+              }}
+              onPress={toogleThemeType}
+              placeholder="Contraseña"
+              secureTextEntry
+              placeholderTextColor={ theme.colors.placeholder}
+            />
+          </View>
+
+          {/* Botón de iniciar sesión */}
+          <Pressable
             style={{
-              width: '100%', marginBottom: 12, backgroundColor: 'white', borderWidth: 1,
-              borderColor: '#d1d5db', padding: 12, borderRadius: 16
+              backgroundColor: theme.colors.primary,
+              width: '100%',
+              paddingVertical: 12,
+              paddingHorizontal: 24,
+              borderRadius: 50
             }}
+            onPress={() => handleLogin(identificador, contrasena, navigation)}
+          >
+            <Text style={{ color: theme.colors.onPrimary, fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>
+              Ingresar
+            </Text>
+          </Pressable>
 
-            placeholder="Username"
-          />
-          <TextInput
-            value={contrasena}
-            onChangeText={setContrasena}
+          {/* Botón para alternar tema */}
+          <Pressable
             style={{
-              width: '100%', marginBottom: 12, backgroundColor: 'white', borderWidth: 1,
-              borderColor: '#d1d5db', padding: 12, borderRadius: 16
+              marginTop: 16,
+              backgroundColor: isDarkTheme ? theme.colors.secondary : theme.colors.primary,
+              paddingVertical: 10,
+              paddingHorizontal: 20,
+              borderRadius: 20
             }}
-            placeholder="Password"
-            secureTextEntry
-          />
-        </View >
-
-        <Pressable
-          style={{ backgroundColor: '#0f172a', width: '100%', paddingVertical: 12, paddingHorizontal: 24, borderRadius: 50 }}
-          onPress={() => handleLogin(identificador, contrasena, navigation)}
-        >
-          <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Iniciar Sesión</Text>
-        </Pressable>
-
+            onPress={toogleThemeType}  // Alterna entre los temas
+          >
+            <Text style={{ color: theme.colors.onPrimary, textAlign: 'center' }}>
+              Cambiar a {isDarkTheme ? 'Modo Día' : 'Modo Noche'}
+            </Text>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
-}
+};
