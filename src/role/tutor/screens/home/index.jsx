7e4d92bd8c3770a8e0641d881e008674264c315e
Gaston <gaston.rodriguez@icloud.com>
2024-10-26 18:19:30 -0500

    banners finished

--- src/role/tutor/screens/home/index.jsx
+++ src/role/tutor/screens/home/index.jsx
@@ -13,6 +13,9 @@ import { ModalBanner } from '../../../../shared/components/modal/modal-banner/in
 const image1 = require('../../../../assets/images/Aviso1.png');
 const image2 = require('../../../../assets/images/Aviso2.png');
 const image3 = require('../../../../assets/images/Aviso3.png');
+const image4 = require('../../../../assets/images/Aviso4.png');
+const image5 = require('../../../../assets/images/Aviso5.png');
+const image6 = require('../../../../assets/images/Aviso6.png');
 
 export const Home = () => {
   const { horarios, getHorariosByGradoSeccion, loading } = useContext(HorariosContext);
@@ -28,9 +31,11 @@ export const Home = () => {
   const images = [
     { source: image1, title: 'Aviso Importante' },
     { source: image2, title: 'Calendario Escolar 2024' },
-    { source: image3, title: 'Cronograma de evaluacione' }
+    { source: image3, title: 'Cronograma de evaluaciones' },
+    { source: image4, title: 'Escuela de familia' },
+    { source: image5, title: 'Campaña solidaria - Surco te abriga' },
+    { source: image6, title: 'Comunicado de Talleres' }
   ];
-
   useEffect(() => {
       setSeccionId(user.perfil.seccion._id);
       setGradoId(user.perfil.grado._id);
@@ -71,20 +76,15 @@ export const Home = () => {
             <Horario horarios={horarios} rol={user.rol} />
           </View>
 
-          <View style={{ 
-            display: isMediumScreen ? 'grid' : 'flex',
-            gridTemplateColumns: 'repeat(3, 1fr)',
-            gap: 12,
-            marginHorizontal: 20,
-            marginBottom: 10
-          }}>
+          <View style={{ flexDirection: 'row', flexWrap: 'wrap', marginHorizontal: 20, marginBottom: 10, gap: 13 }}>
             {images.map((img, index) => (
-              <Banner
-                key={index}
-                title={img.title}
-                onPress={() => handleOpenModal(img.source, img.title)}
-                source={img.source}
-              />
+              <View key={index} style={{ width: isMediumScreen ? '32%' : '100%', marginBottom: 12 }}> 
+                <Banner
+                  title={img.title}
+                  onPress={() => handleOpenModal(img.source, img.title)}
+                  source={img.source}
+                />
+              </View>
             ))}
           </View>
 
